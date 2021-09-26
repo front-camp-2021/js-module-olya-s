3f3547e61494ea69888b1ba61aea80cce833eb27
olya-s <osafronyuk85@gmail.com>
2021-09-27 02:25:19 +0300

    solved tasks

--- src/module-1/debounce/index.js
+++ src/module-1/debounce/index.js
@@ -1,5 +1,12 @@
 export const debounce = (fn, delay = 0) => {
-
+  let isIgnored = true;
+  return function () {
+    if (isIgnored) {
+      isIgnored = false;
+      setTimeout(() => isIgnored = true, delay);
+      return fn.apply(this, arguments);
+    }
+    else
+      return;
+  }
 }
-
-
--- src/module-1/splitAndMerge/index.js
+++ src/module-1/splitAndMerge/index.js
@@ -1,1 +1,6 @@
-export const splitAndMerge = (str = "", separator = "") => {};
+export const splitAndMerge = (str = "", separator = "") => {
+  let newStr = str.split(' ')
+    .map(word => word.split('').join(separator))
+    .join(' ');
+  return newStr;
+};
