a7b7d721daec864ee14c550d2fb9e4cf56e18ebb
olya-s <osafronyuk85@gmail.com>
2021-09-29 01:47:31 +0300

    cardList without update

--- src/module-3/cards-list-v1/index.js
+++ src/module-3/cards-list-v1/index.js
@@ -1,9 +1,33 @@
 import Card from '../../module-2/card/index.js';
 
 export default class CardsList {
-  constructor (data = []) {
+  constructor(data = []) {
     this.data = data;
+    this.element;
 
-    // ... your logic
+    this.render();
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove()
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
+
+  render() {
+    const ul = document.createElement('ul');
+    ul.className = 'catalog';
+    this.data.forEach(item => {
+      const li = document.createElement('li');
+      li.append((new Card(item)).element);
+      ul.append(li);
+    });
+    this.element = ul;
+    return this.element;
   }
 }
