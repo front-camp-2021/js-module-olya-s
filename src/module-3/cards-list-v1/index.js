7947d397e441c7210a3c90f3aa318355bd4c0b6b
olya-s <osafronyuk85@gmail.com>
2021-10-01 21:57:06 +0300

    done cards-list

--- src/module-3/cards-list-v1/index.js
+++ src/module-3/cards-list-v1/index.js
@@ -1,10 +1,50 @@
 export default class CardsList {
+  element;
+  subElements = {};
+
   constructor({ data = [], Component = {} }) {
     this.data = data;
     this.Component = Component;
-    this.element;
 
     this.render();
+    this.getSubElements();
+    this.update(this.data);
+  }
+
+  get template() {
+    return (
+      `<div>
+        <div class="catalog" data-element="body">
+        </div>
+      </div>`
+    )
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.template;
+    this.element = wrapper.firstElementChild;
+  }
+
+  getSubElements() {
+    const result = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+    this.subElements = result;
+  }
+
+  update(data) {
+    this.data = data;
+    const cards = data.map(item => new this.Component(item).element);
+    if (cards.length) {
+      this.subElements.body.replaceChildren(...cards);
+    }
+    else {
+      this.subElements.body.innerText = "No products found";
+    }
   }
 
   remove() {
@@ -16,27 +56,5 @@ export default class CardsList {
   destroy() {
     this.remove();
     this.element = null;
-  }
-
-  update(cardsList) {
-    if (!this.element) return;
-  }
-
-  render() {
-    const ul = document.createElement('ul');
-    ul.className = 'catalog';
-    if (!this.data.length) return;
-    this.createCardsList(ul, thiis.data);
-    this.element = ul;
-    return this.element;
-  }
-
-  createCardsList(container, list) {
-    if (!list.length) return;
-    list.forEach(item => {
-      const li = document.createElement('li');
-      li.append((new this.Component(item)));
-      container.append(li);
-    });
   }
 }
