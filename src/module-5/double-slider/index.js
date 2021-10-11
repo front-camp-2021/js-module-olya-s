9388c35dc16e69d31c86558548b83c13ae8e09c4
olya-s <osafronyuk85@gmail.com>
2021-10-11 12:03:24 +0300

    slider in process, pagination done

--- src/module-5/double-slider/index.js
+++ src/module-5/double-slider/index.js
@@ -1,4 +1,71 @@
 export default class DoubleSlider {
+  element;
+  subElements = {};
+
+  pointerDown = (event) => {
+    if (event.target.className.includes('thumb')) {
+      this.element.classList.add('range-slider_dragging');
+      this.slider = event.target;
+      this.outLeftX = this.subElements.sliderInner.getBoundingClientRect().left;
+      this.outRightX = this.subElements.sliderInner.getBoundingClientRect().right;
+      this.thumbLeftX = this.subElements.thumbLeft.getBoundingClientRect().right;
+      this.thumbRightX = this.subElements.thumbRight.getBoundingClientRect().left;
+
+      if (event.target === this.subElements.thumbLeft) {
+        this.shiftX = event.pageX - this.slider.getBoundingClientRect().right;
+      } else {
+        this.shiftX = event.pageX - this.slider.getBoundingClientRect().left;
+      }
+      this.slider.style.zIndex = 1000;
+      document.addEventListener('pointermove', this.pointerMove);
+    }
+  };
+
+  pointerMove = (event) => {
+    const progress = this.subElements.progress;
+
+    if (this.slider === this.subElements.thumbLeft) {
+      let posX = event.pageX - this.shiftX;
+      if (posX < this.outLeftX) {
+        posX = this.outLeftX;
+      } else if (posX > this.thumbRightX) {
+        posX = this.thumbRightX;
+      }
+      this.selected.from = this.min + this.getValueInPercent(posX - this.outLeftX) * (this.max - this.min) / 100;
+    }
+    if (this.slider === this.subElements.thumbRight) {
+      let posX = event.pageX - this.shiftX;
+      if (posX < this.thumbLeftX) {
+        posX = this.thumbLeftX;
+      } else if (posX > this.outRightX) {
+        posX = this.outRightX;
+      }
+      this.selected.to = this.max - this.getValueInPercent(this.outRightX - posX) * (this.max - this.min) / 100;
+    }
+    this.update();
+  }
+
+  pointerUp = (event) => {
+    this.element.classList.remove('range-slider_dragging');
+    if (this.slider) {
+      document.removeEventListener('pointermove', this.pointerMove);
+      document.removeEventListener('pointerdown', this.pointerDown);
+    }
+
+    this.element.dispatchEvent(new CustomEvent('range-selected',
+      {
+        bubbles: true,
+        detail: {
+          value: this.selected
+        }
+      })
+    );
+  };
+
+  onDragStart = () => {
+    return false;
+  };
+
   constructor({
     min = 100,
     max = 200,
@@ -10,6 +77,91 @@ export default class DoubleSlider {
     precision = 0,
     filterName = ''
   } = {}) {
-    // ... your logic
+    this.min = min;
+    this.max = max;
+    this.formatValue = formatValue;
+    this.selected = selected;
+    this.precision = precision;
+    this.filterName = filterName;
+
+    this.render();
+    this.getSubElements();
+    this.addEventListeners();
+  }
+
+  get template() {
+    const left = (this.selected.from - this.min) * 100 / (this.max - this.min);
+    const right = (this.max - this.selected.to) * 100 / (this.max - this.min);
+    return `<div class="range-slider" data-element="rangeSlider">
+        <span data-element="from">${this.formatValue(this.selected.from)}</span>
+        <div class="range-slider__inner" data-element="sliderInner">
+          <span data-element="progress" class="range-slider__progress"
+            style="left: ${left}%; right: ${right}%"></span>
+          <span data-element="thumbLeft" class="range-slider__thumb-left"
+            style="left: ${left}%"></span>
+          <span data-element="thumbRight" class="range-slider__thumb-right"
+            style="right: ${right}%"></span>
+        </div>
+        <span data-element="to">${this.formatValue(this.selected.to)}</span>
+      </div >`
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.template;
+    this.element = wrapper.firstElementChild;
+    return this.element;
+  }
+
+  remove() {
+    if (this.element) {
+      this.removeEventListeners();
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
+
+  update() {
+    const from = this.selected.from;
+    const to = this.selected.to;
+    const posFrom = from - this.min;
+    const posTo = this.max - to;
+    this.subElements.thumbLeft.style.left = `${posFrom}%`;
+    this.subElements.progress.style.left = `${posFrom}%`;
+    this.subElements.from.innerText = this.formatValue(this.selected.from);
+    this.subElements.thumbRight.style.right = `${posTo}%`;
+    this.subElements.progress.style.right = `${posTo}%`;
+    this.subElements.to.innerText = this.formatValue(this.selected.to);
+  }
+
+  getSubElements() {
+    const result = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+    this.subElements = result;
+  }
+
+  getValueInPercent(value) {
+    const sliderWidth = this.subElements.sliderInner.getBoundingClientRect().width;
+    return Math.floor(value * 100 / sliderWidth);
+  }
+
+  addEventListeners() {
+    this.element.addEventListener('pointerdown', this.pointerDown);
+    document.addEventListener('pointerup', this.pointerUp);
+    document.addEventListener('ondragstart', this.onDragStart);
+  }
+
+  removeEventListeners() {
+    this.element.removeEventListener('pointerdown', this.pointerDown);
+    document.removeEventListener('pointerup', this.pointerUp);
+    document.removeEventListener('ondragstart', this.onDragStart);
   }
 }
--- src/module-5/pagination/index.js
+++ src/module-5/pagination/index.js
@@ -1,31 +1,172 @@
 export default class Pagination {
   element;
-  start = 0;
-  pageIndex = 0;
+  subElements = {};
+  start = 1;
+
+  onClick = event => {
+    if (event.target.classList.contains('pagination__link')) {
+      if (event.target.parentElement.dataset.element === 'prevPage') {
+        this.goToPrevPage();
+      } else if (event.target.parentElement.dataset.element === 'nextPage') {
+        this.goToNextPage();
+      }
+      if (event.target.className === 'pagination__link') {
+        this.update(+event.target.dataset.index);
+      }
+    }
+  }
 
   constructor({
     totalPages = 10,
     page = 1,
+    currentPage = page,
+    viewPages = 9
   } = {}) {
     this.totalPages = totalPages;
-    this.pageIndex = page - 1;
+    this.currentPage = currentPage;
+    this.viewPages = viewPages;
 
-    // ... your logic
+    this.render();
+    this.addEventListeners();
+    this.getSubElements();
   }
 
-  goToPrevPage () {
-    // ... your logic
+  goToPrevPage() {
+    this.update(this.currentPage - 1);
   }
 
-  goToNextPage () {
-    // ... your logic
+  goToNextPage() {
+    this.update(this.currentPage + 1);
   }
 
-  remove () {
-    // ... your logic
+  remove() {
+    if (this.element) {
+      this.removeEventListeners();
+      this.element.remove();
+    }
   }
 
-  destroy () {
-    // ... your logic
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
+  }
+
+  get template() {
+    return `<div class="pagination">
+        <span class="pagination__item" data-element="prevPage">
+          ${this.currentPage === 1 ? `&lt;` :
+        `<a class="pagination__link pagination__link_arrow" href="#">&lt;</a>`}
+        </span>
+        <ul class="pagination__list" data-element="pagesList">
+        ${this.getPages()}
+        </ul>
+        <span class="pagination__item" data-element="nextPage">
+          ${this.currentPage === this.totalPages ? `&gt;` :
+        `<a class="pagination__link pagination__link_arrow" href="#">&gt;</a>`}
+        </span>
+      </div>`
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.template;
+    this.element = wrapper.firstElementChild;
+    return this.element;
+  }
+
+  update(number) {
+    this.currentPage = number;
+    if (this.currentPage === 1) {
+      this.subElements.prevPage.innerHTML = '&lt;';
+    } else {
+      this.subElements.prevPage.innerHTML = `<a class="pagination__link pagination__link_arrow" href="#">&lt;</a>`
+    }
+    if (this.currentPage === this.totalPages) {
+      this.subElements.nextPage.innerHTML = '&gt;';
+    } else {
+      this.subElements.nextPage.innerHTML = `<a class="pagination__link pagination__link_arrow" href="#">&gt;</a>`
+    }
+    this.subElements.pagesList.innerHTML = this.getPages();
+
+    this.element.dispatchEvent(new CustomEvent('page-changed', {
+      detail: {
+        page: number
+      }
+    }));
+  }
+
+  getSubElements() {
+    const elements = this.element.querySelectorAll('[data-element]');
+    const result = {};
+    for (let subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+    this.subElements = result;
+  }
+
+  getPages() {
+    const pages = [];
+    if (this.currentPage < this.start + 5) {
+      for (let i = this.start; i < this.viewPages - 1; i++) {
+        if (this.currentPage === i) {
+          pages[i] = `<li class="pagination__item pagination__item_current" 
+            data-element="current" data-index=${i}>${i}</li>`;
+        } else {
+          pages[i] = `<li class="pagination__item">
+            <a class="pagination__link" href="#" data-index=${i}>${i}</a>
+            </li>`;
+        }
+      }
+      pages[this.start + 7] = `<li class="pagination__item">
+        <a class="pagination__link" href="#" data-index=${this.start + 7}>...</a>
+        </li>`;
+      pages[this.viewPages] = `<li class="pagination__item">
+        <a class="pagination__link" href="#" data-index=${this.totalPages}>${this.totalPages}</a>
+        </li>`;
+    } else if (this.currentPage > this.totalPages - 5) {
+      for (let i = this.totalPages; i > this.totalPages - this.viewPages + 1; i--) {
+        if (this.currentPage === i) {
+          pages[i] = `<li class="pagination__item pagination__item_current" 
+            data-element="current" data-index=${i}>${i}</li>`;
+        } else {
+          pages[i] = `<li class="pagination__item">
+            <a class="pagination__link" href="#" data-index=${i}>${i}</a></li>`;
+        }
+      }
+      pages[this.totalPages - this.viewPages + 2] = `<li class="pagination__item">
+        <a class="pagination__link" href="#" data-index=${this.totalPages - this.viewPages + 2}>...</a>
+        </li>`;
+      pages[this.start] = `<li class="pagination__item">
+        <a class="pagination__link" href="#" data-index=${this.start}>${this.start}</a></li>`;
+    } else {
+      pages[this.start] = `<li class="pagination__item">
+        <a class="pagination__link" href="#" data-index=${this.start}>${this.start}</a></li>`;
+      pages[this.currentPage - 3] = `<li class="pagination__item">
+        <a class="pagination__link" href="#" data-index=${this.currentPage - 3}>...</a></li>`;
+      for (let i = this.currentPage - 2; i <= this.currentPage + 2; i++) {
+        if (this.currentPage === i) {
+          pages[i] = `<li class="pagination__item pagination__item_current" 
+            data-element="current" data-index=${i}>${i}</li>`;
+        } else {
+          pages[i] = `<li class="pagination__item">
+            <a class="pagination__link" href="#" data-index=${i}>${i}</a></li>`;
+        }
+      }
+      pages[this.totalPages] = `<li class="pagination__item">
+      <a class="pagination__link" href="#" data-index=${this.totalPages}>${this.totalPages}</a></li>`;
+      pages[this.currentPage + 3] = `<li class="pagination__item">
+        <a class="pagination__link" href="#" data-index=${this.currentPage + 3}>...</a></li>`;
+    }
+    return pages.join('');
+  }
+
+  addEventListeners() {
+    this.element.addEventListener('click', this.onClick);
+  }
+
+  removeEventListeners() {
+    this.element.removeEventListener('click', this.onClick);
   }
 }
